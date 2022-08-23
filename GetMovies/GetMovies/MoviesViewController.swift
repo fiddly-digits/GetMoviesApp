71db1e4411c730cb87ec5e2546f6be21583bbd35
fiddly-digits <rob.crlu@gmail.com>
2022-08-23 16:45:37 -0500

    Interface Design, Movie Cell, and view controller files

---
+++ GetMovies/GetMovies/MoviesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesViewController.swift
+//  GetMovies
+//
+//  Created by Roberto Cruz on 23/08/22.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ GetMovies/GetMovies/SeriesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SeriesViewController.swift
+//  GetMovies
+//
+//  Created by Roberto Cruz on 23/08/22.
+//
+
+import UIKit
+
+class SeriesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
